beecc0540051800212c720f0d840ea8e00a1d14b
Theodora2000 <xsimont@stuba.sk>
2021-04-14 16:35:49 +0200

    2.3

--- main.c
+++ main.c
@@ -3,11 +3,40 @@
 #include <memory.h>
 #include <stdlib.h>
 
-int* fun(int pole[], int dlzka){
-    for(int i=1;i<5;i++){
-        printf("%d ", pole[i]);
+int* fun(int pole[], int dlzka, int n){
+    int *nove;
+    nove = (int*)malloc((dlzka-1)*sizeof(int));
+    int j=0;
+    for(int i=1;i<dlzka;i++){
+        nove[j]=pole[i];
+        j++;
+    }
+    nove = realloc(nove,(dlzka-1+n)*sizeof(int));
+    for(int i=0;i<n;i++){
+        nove[j]=i+j;
     }
 
+    printf("Stare pole\n");
+    for(int i=1;i<dlzka;i++){
+        printf("%d ",pole[i]);
+
+    }
+    printf("\nNove pole\n");
+    for(int i=0;i<j;i++){
+        printf("%d ", nove[i]);
+    }
+
+    nove = realloc(nove,(dlzka-1+n)*sizeof(int));
+    for(int i=0;i<n;i++){
+        nove[j]=i+j;
+        j++;
+    }
+
+
+    printf("\nNove pole\n");
+    for(int i=0;i<j;i++){
+        printf("%d ", nove[i]);
+    }
 
 
     return pole;
@@ -15,10 +44,10 @@ int* fun(int pole[], int dlzka){
 }
 
 int main() {
-    int pole[]={4,1,9,7,3};
+    int pole[]={10,1,9,7,3,4,7,2,5,7,6};
 
     int dlzka = sizeof(pole)/sizeof(pole[0]);
-
-    fun(pole,dlzka);
+    int n=100;
+    fun(pole,dlzka, n);
     return 0;
 }
